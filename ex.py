481eb72d05a8236cf5ce6b835a413c803085e32c
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-07-03 18:31:33 -0400

    meet

---
+++ ex.py
@@ -0,0 +1,8 @@
+inch = int(input("Enter the amount of inches to convert to feet: "))
+
+def ft(inch):
+    return inch / 12
+
+feet = ft(inch)
+
+print("The measurement of feet is", feet)
---
+++ meeting.py
@@ -0,0 +1,9 @@
+man = str(input("What is your name? "))
+man = man.lower()
+if man == "Shea" or man == "shea":
+    print("Hello " + man)
+    print("Cool")
+elif man == "john":
+    print("Hello " + man)
+else:
+    print("You don't belong here!")
