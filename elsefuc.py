07d12ece513f521083c08fd4592951d6824b6326
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-07-05 13:05:58 -0400

    guess the fruit

---
+++ elsefuc.py
@@ -0,0 +1,8 @@
+def frt():
+    frt = input("Enter the name of a fruit I am thinking of: ")
+    if frt.lower() == "Orange":
+        print("You guessed it!")
+    else:
+        print("Incorrect")
+
+frt()
